6daa8517198332da51e734083a7ade38f07f111f
ChanYatChunSunny <124800646+ChanYatChunSunny@users.noreply.github.com>
2024-08-09 14:03:54 +0800

    Get and set UI

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -25,6 +26,35 @@ public class Spawner : MonoBehaviour
     // Inner array is to be assigned to each type
     private float[][] attractionForces;
     private float[][] maxDetectionDistances;
+    private bool configChanged;
+
+    // Side-effect of getting this property: Will set it to false
+    public bool ConfigChanged
+    {
+        get { bool ret = configChanged;  configChanged = false; return ret; }
+        private set { configChanged = value; }
+    }
+
+    public float GetAttractionForce(int i , int j)
+    {
+        return attractionForces[i][j];
+    }
+    public void SetAttractionForce(int i, int j, float force)
+    {
+        attractionForces[i][j] = force;
+        configChanged = true;
+    }
+
+    public float GetMaxDetectionDistance(int i , int j)
+    {
+        return maxDetectionDistances[i][j];
+    }
+    public void SetMaxDetectionDistance(int i, int j, float distance)
+    {
+        maxDetectionDistances[i][j] = distance;
+        configChanged = true;
+    }
+
 
     private float[] randMins;
     private float[] randMaxs;
@@ -44,8 +74,12 @@ public class Spawner : MonoBehaviour
         {
             attractionForces[i] = new float[numOfTypes];
             maxDetectionDistances[i] = new float[numOfTypes];
+            for(int j = 0; j < numOfTypes; j++)
+            {
+                attractionForces[i][j] = 0f;
+                maxDetectionDistances[i][j] = 0f;
+            }
         }
-
         // Init the partitions
         int partitionCount = partitioner.GetPartitionCount();
         partitionedParticles = new LinkedList<GameObject>[partitionCount][][];
@@ -69,16 +103,20 @@ public class Spawner : MonoBehaviour
             }
             for (int j = 0; j < numOfTypes; j++)
             {
-                attractionForces[i][j] = randomizer.GetInt(-128, 129);
-                maxDetectionDistances[i][j] = randomizer.GetInt(0, 33);
+                attractionForces[i][j] = (float)randomizer.GetDouble(-128, 129);
+                maxDetectionDistances[i][j] = (float)randomizer.GetDouble(0, 49);
             }
         }
+        ConfigChanged = true;
     }
 
-    public void Spawn(int amount) 
+    public void Spawn(int amount, bool isRandConfig) 
     {
+        if (isRandConfig)
+        {
+            RandomizeTypes();
+        }
 
-        RandomizeTypes();
         int partitionCount = partitioner.GetPartitionCount();
 
         for (int i = 0; i < partitionCount; i++) 
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
@@ -12,22 +13,52 @@ public class UIController : MonoBehaviour
     private TMP_InputField seedField;
     [SerializeField]
     private TMP_InputField sizeField;
+    [SerializeField]
+    private TMP_Text[] forceLabels;
+    [SerializeField]
+    private TMP_Text[] distanceLabels;
+
+    // The sliders IO here follow the IPO direction of user -> slider -> script, script -> slider -> user
+    // So when user input, it is the responsibility of the slider to call the script, not the script to monitor
+    [SerializeField]
+    private Slider[] forceSliders;
+    [SerializeField]
+    private Slider[] distanceSliders;
+
+    [SerializeField]
+    private Toggle randConfigToggle;
 
     [SerializeField]
     private GameObject randomizerObj;
+
     [SerializeField]
     private GameObject spawnerObj;
+    private Spawner spawner;
+
+    private int selectedType;
 
     // Start is called before the first frame update
     void Start()
     {
+        spawner = spawnerObj.GetComponent<Spawner>();
         wrongText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (spawner.ConfigChanged)
+        {
+           for(int i = 0; i < 8; i++)
+            {
+                float force = spawner.GetAttractionForce(selectedType, i);
+                forceSliders[i].value = force;
+                forceLabels[i].text = force.ToString("0.00");
+                float distance = spawner.GetMaxDetectionDistance(selectedType, i);
+                distanceSliders[i].value = distance;
+                distanceLabels[i].text = distance.ToString("0.00");
+            }
+        }
     }
 
     public void PressStart() 
@@ -40,9 +71,8 @@ public class UIController : MonoBehaviour
             if (seed > 0 && size > 0) 
             {
                 randomizerObj.GetComponent<Randomizer>().SetSeed(seed);
-                Spawner spawner = spawnerObj.GetComponent<Spawner>();
                 spawner.Clear();
-                spawner.Spawn(size);
+                spawner.Spawn(size, randConfigToggle.isOn);
             }
             else
             {
@@ -53,5 +83,23 @@ public class UIController : MonoBehaviour
         {
             wrongText.gameObject.SetActive(true);
         }
+    }
+    public void SelectParticleType(int type)
+    {
+        selectedType = type;
+        for (int i = 0; i < 8; i++)
+        {
+            forceSliders[i].value = spawner.GetAttractionForce(type, i);
+            distanceSliders[i].value = spawner.GetMaxDetectionDistance(type, i);
+        }
+    }
+    public void SetForce(Slider slider)
+    {
+        spawner.SetAttractionForce(selectedType, int.Parse(slider.gameObject.name.Replace("Force", "").Replace("Slider", "")), slider.value);
+    }
+
+    public void SetDistance(Slider slider)
+    {
+        spawner.SetMaxDetectionDistance(selectedType, int.Parse(slider.gameObject.name.Replace("Distance", "").Replace("Slider", "")), slider.value);
     }
 }
